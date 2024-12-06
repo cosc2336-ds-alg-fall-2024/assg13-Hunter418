9793e16f6875cc77c40aac0323f6a049738234dd
Hunter418 <hwilliams31@leomail.tamuc.edu>
2024-12-06 20:43:19 +0000

    commit to track set docs
    this is the worst kind of assigment for me to try to do, dont expect much, im nothing without instructions

---
+++ src/set.cpp
@@ -0,0 +1,16 @@
+/** @file set.cpp
+ * @brief attempting to do this assigment
+ *
+ * @author Hunter l williams
+ * @note   class: COSC 2336, fall 2024
+ * @note   ide  : VSCode Server 3.9.3, Gnu Development Tools
+ * @note   assg : Assignment 13
+ * @date   Dec 6th, 2024
+ *
+ *
+ */
+
+#include <iostream>
+#include <set.hpp>
+#include <vector>
+using namespace std;
