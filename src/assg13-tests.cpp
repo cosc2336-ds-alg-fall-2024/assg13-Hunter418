c2e283bb36c7a92d17b5fa35fabb2e65470dde1e
Hunter418 <hwilliams31@leomail.tamuc.edu>
2024-12-07 06:31:41 +0000

    Project is semi-complete.
    I am having issues with my make file but other than that I am pretty sure this will run as expected. I re-implemented the queue class.

--- src/assg13-tests.cpp
+++ src/assg13-tests.cpp
@@ -10,9 +10,13 @@
  *
  * Assignment Standard Template Library STL.
  */
-#include <list>
-#include <iostream>
+#include "QueueException.hpp"
 #include "catch.hpp"
+#include "set.hpp"
+#include <iostream>
+#include <list>
+#include <queue>
+
 using namespace std;
 
 /** Task 1 example test task.  Demonstarte using an STL list
@@ -31,5 +35,43 @@ TEST_CASE("<list> test list STL container", "[task1]")
   CHECK_FALSE(l1.empty());
   CHECK(l1.size() == 1);
 }
+TEST_CASE("Test front() function")
+{
+  std::queue<int> q;
 
+  enqueue(q, 10);
+  enqueue(q, 20);
+  enqueue(q, 30);
 
+  CHECK(front(q) == 10); // Front of the queue should be 10
+
+  clear(q);
+  CHECK_THROWS_AS(front(q), QueueEmptyException); // Front on an empty queue should throw
+}
+
+TEST_CASE("Test enqueue() function")
+{
+  std::queue<int> q;
+
+  enqueue(q, 10);
+  enqueue(q, 20);
+
+  CHECK(front(q) == 10); // Front of the queue should still be 10
+  CHECK(q.size() == 2);  // Queue size should be 2
+}
+
+TEST_CASE("Test dequeue() function")
+{
+  std::queue<int> q;
+
+  enqueue(q, 10);
+  enqueue(q, 20);
+
+  dequeue(q);
+  CHECK(front(q) == 20); // Front of the queue should now be 20
+
+  dequeue(q);
+  CHECK(q.empty()); // Queue should be empty
+
+  CHECK_THROWS_AS(dequeue(q), QueueEmptyException); // Dequeue on an empty queue should throw
+}
--- src/set.cpp
+++ src/set.cpp
@@ -1,7 +1,7 @@
 /** @file set.cpp
  * @brief attempting to do this assigment
  *
- * @author Hunter l williams
+ * @author Hunter l Williams
  * @note   class: COSC 2336, fall 2024
  * @note   ide  : VSCode Server 3.9.3, Gnu Development Tools
  * @note   assg : Assignment 13
@@ -9,8 +9,110 @@
  *
  *
  */
-
+#include "QueueException.hpp"
 #include <iostream>
+#include <queue>
 #include <set.hpp>
+#include <stdexcept>
 #include <vector>
 using namespace std;
+
+/**
+ * @brief Front accesor
+ *
+ *  Retrieves the element at the front of the queue without removing it.
+ *
+ *  @param queue The queue from which the front element will be retrieved
+ *
+ *  @returns The value of the element at the front of the queue
+ *
+ */
+int front(const std::queue<int>& queue)
+{
+  if (!queue.empty())
+  {
+    return queue.front();
+  }
+  else
+  {
+    throw QueueEmptyException("Queue is empty. Cannot access front.");
+  }
+}
+
+/**
+ * @brief Clear out the queue
+ *
+ * Clear or empty out the queue.  Return the queue back
+ * to an empty queue.
+ *
+ * @param queue The queue to be cleared of all elements.
+ *
+ * @returns Nothing, because it is a void function.\
+ *
+ */
+void clear(std::queue<int>& queue)
+{
+  while (!queue.empty())
+  {
+    queue.pop();
+  }
+}
+
+/**
+ * @brief Dequeue
+ *
+ * Removes the front element from the queue, as long as the queue isn't empty.
+ *
+ * @param queue The queue from which the front element will be removed
+ *
+ * @returns nothing because it is a void function
+ *
+ */
+void dequeue(std::queue<int>& queue)
+{
+  if (!queue.empty())
+  {
+    queue.pop();
+  }
+  else
+  {
+    throw QueueEmptyException("Queue is empty. Cannot dequeue.");
+  }
+}
+/**
+ * @brief equeue
+ *
+ * inserts an item to the back of the queue
+ *
+ * @param queue The queue where the new element will be added.
+ *
+ * @param value The value to be added to the back of the queue.
+ *
+ * @returns nothing because it is a void function
+ *
+ */
+void enqueue(std::queue<int>& queue, int value)
+{
+  queue.push(value);
+}
+
+/**
+ * @brief Merge two queues
+ *
+ * Appends all elements from the second queue to the first queue.
+ *
+ * @param queue1 The first queue
+ * @param queue2 The second queue
+ *
+ * @returns Nothing, because it is a void function.
+ */
+void mergeQueues(std::queue<int>& queue1, const std::queue<int>& queue2)
+{
+  queue<int> tempQueue = queue2;
+
+  while (!tempQueue.empty())
+  {
+    queue1.push(tempQueue.front());
+    tempQueue.pop();
+  }
+}
